3669084f0d97796337ff1459b84e74439c132fe4
Mary <ehhfs@mail.ru>
2023-09-03 02:49:44 +0900

    axios was imported and url created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1ea16ea4c9b44c6553072e400bbea74c`;
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
