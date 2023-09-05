73d7313b54c105c2f8997cc71bfdd81dc35873e3
Mary <ehhfs@mail.ru>
2023-09-05 23:59:19 +0900

    html added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,36 @@ function App() {
 
   // const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1ea16ea4c9b44c6553072e400bbea74c`;
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>London</p>
+          </div>
+          <div className="temp">
+            <h1>10</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>10°C</p>
+          </div>
+          <div className="humidity"></div>
+          <div className="wind"></div>
+        </div>
+      </div>
     </div>
   );
 }
